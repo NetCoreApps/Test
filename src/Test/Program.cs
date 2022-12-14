b616f33760fdeea6f1403c797bca2a4f6fd46ebf
Darren Reid <darren@reidmail.org>
2022-12-14 15:14:56 +1100

    Add OSS license.

--- src/Test/Program.cs
+++ src/Test/Program.cs
@@ -1,8 +1,9 @@
 using ServiceStack;
 
+Licensing.RegisterLicense("OSS BSD-3-Clause 2022 https://github.com/NetCoreApps/Test u95ySKVHFfNYZKUaUtvFTP8/+oaZLQBjcyQQA6enXR7yf5UxPRRfafgy4tiXdSck8Nwlxs5pRfTOxJN+ht64dkus9x37IVBrbzkX2WNsBRNzow9eCIZI+PYmdSj3vSNC5+/QagOnj6nhD30jx6mCx/b8qxRdv8x+rv5YRN7afnY=");
+
 var builder = WebApplication.CreateBuilder(args);
 
-Licensing.RegisterLicense(Environment.GetEnvironmentVariable("SERVICESTACK_LICENSE"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
