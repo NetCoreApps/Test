9638a6e7bfe9bbbf791229f5d14f09aaeaf0b2be
Demis Bellot <demis.bellot@gmail.com>
2024-02-28 11:03:08 +0800

    remove ServiceStack.Api.OpenApi

--- Test.ServiceInterface/PingService.cs
+++ Test.ServiceInterface/PingService.cs
@@ -5,106 +5,104 @@ using System.Linq;
 using ServiceStack;
 using ServiceStack.OrmLite;
 
-namespace Test.ServiceInterface
+namespace Test.ServiceInterface;
+
+[Route("/ping")]
+public class Ping { }
+
+public class PingResponse
 {
-    [Route("/ping")]
-    public class Ping { }
+    public Dictionary<string, ResponseStatus> Responses { get; set; }
+    public ResponseStatus ResponseStatus { get; set; }
+}
 
-    public class PingResponse
+public class PingService : Service
+{
+    private static string[] urls = new[] 
     {
-        public Dictionary<string, ResponseStatus> Responses { get; set; }
-        public ResponseStatus ResponseStatus { get; set; }
-    }
+        "http://techstacks.io",
+        "http://techstacks.io/tasks/hourly",
+        "http://autoqueryviewer.servicestack.net/services",
+        "http://github.servicestack.net/repos",
+        "https://servicestack.net/pricing",
+        "http://react-chat.servicestack.net",
+        "http://chat.servicestack.net",
+        //"http://badapi.net/internalservererror",
+        "http://stackapis.servicestack.net",
+        "http://httpbenchmarks.servicestack.net",
+        "http://emailcontacts.servicestack.net",
+        "http://imgur.servicestack.net",
+        //"http://bootstrapapi.servicestack.net",
+        "http://todos.servicestack.net",
+        "http://razor.servicestack.net",
+        "http://redisadminui.servicestack.net",
+        "http://redisreact.servicestack.net",
+        "http://awsrazor.servicestack.net",
+        "http://awsapps.servicestack.net",
+        "http://awsrazor.servicestack.net/stars/dead/cobain/",
+        "http://redisgeo.servicestack.net",
+        //"http://restfiles.servicestack.net",
+        //"http://redisstackoverflow.servicestack.net",
+        //"http://northwind.servicestack.net",
+        //"http://docs.servicestack.net",
+        //"http://mvc.servicestack.net",
+        //"http://webforms.servicestack.net",
+    };
 
-    public class PingService : Service
+    public object Any(Ping request)
     {
-        private static string[] urls = new[] 
+        var ret = new PingResponse
         {
-            "http://techstacks.io",
-            "http://techstacks.io/tasks/hourly",
-            "http://autoqueryviewer.servicestack.net/services",
-            "http://github.servicestack.net/repos",
-            "https://servicestack.net/pricing",
-            "http://react-chat.servicestack.net",
-            "http://chat.servicestack.net",
-            //"http://badapi.net/internalservererror",
-            "http://stackapis.servicestack.net",
-            "http://httpbenchmarks.servicestack.net",
-            "http://emailcontacts.servicestack.net",
-            "http://imgur.servicestack.net",
-            //"http://bootstrapapi.servicestack.net",
-            "http://todos.servicestack.net",
-            "http://razor.servicestack.net",
-            "http://redisadminui.servicestack.net",
-            "http://redisreact.servicestack.net",
-            "http://awsrazor.servicestack.net",
-            "http://awsapps.servicestack.net",
-            "http://awsrazor.servicestack.net/stars/dead/cobain/",
-            "http://redisgeo.servicestack.net",
-            //"http://restfiles.servicestack.net",
-            //"http://redisstackoverflow.servicestack.net",
-            //"http://northwind.servicestack.net",
-            //"http://docs.servicestack.net",
-            //"http://mvc.servicestack.net",
-            //"http://webforms.servicestack.net",
+            Responses = new Dictionary<string, ResponseStatus>()
         };
 
-        public object Any(Ping request)
+        foreach (var url in urls)
         {
-            var ret = new PingResponse
+            var name = url.Replace("https", "http").Replace("http://", "").SplitOnFirst('.').First();
+            try
             {
-                Responses = new Dictionary<string, ResponseStatus>()
-            };
-
-            foreach (var url in urls)
-            {
-                var name = url.Replace("https", "http").Replace("http://", "").SplitOnFirst('.').First();
-                try
+                var sw = Stopwatch.StartNew();
+                var response = url.GetStringFromUrl(requestFilter: req => req.With(c => c.UserAgent = "TestServices"));
+                ret.Responses[name] = new ResponseStatus
                 {
-                    var sw = Stopwatch.StartNew();
-                    var response = url.GetStringFromUrl(requestFilter: req => req.With(c => c.UserAgent = "TestServices"));
-                    ret.Responses[name] = new ResponseStatus
-                    {
-                        Message = "OK {0}ms".Fmt(sw.ElapsedMilliseconds)
-                    };
-                }
-                catch (Exception ex)
-                {
-                    ret.Responses[name] = ex.ToResponseStatus();
-                    var body = ex.GetResponseBody();
-                    ret.Responses[name].Message = body;
-                }
+                    Message = "OK {0}ms".Fmt(sw.ElapsedMilliseconds)
+                };
             }
-
-            ret.ResponseStatus = ret.Responses
-                .Where(x => x.Value.ErrorCode != null)
-                .Select(x => x.Value)
-                .FirstOrDefault();
-
-            if (ret.ResponseStatus != null)
+            catch (Exception ex)
             {
-                Response.StatusCode = 500;
-
-                try {
-                    Any(new ResetConnections());
-                } catch {}
+                ret.Responses[name] = ex.ToResponseStatus();
+                var body = ex.GetResponseBody();
+                ret.Responses[name].Message = body;
             }
-
-            return ret;
         }
 
-        [Route("/reset-connections")]
-        public class ResetConnections {}
+        ret.ResponseStatus = ret.Responses
+            .Where(x => x.Value.ErrorCode != null)
+            .Select(x => x.Value)
+            .FirstOrDefault();
 
-        public object Any(ResetConnections request)
+        if (ret.ResponseStatus != null)
         {
-            return Db.Column<bool>(
-                @"SELECT pg_terminate_backend(pid)
+            Response.StatusCode = 500;
+
+            try {
+                Any(new ResetConnections());
+            } catch {}
+        }
+
+        return ret;
+    }
+
+    [Route("/reset-connections")]
+    public class ResetConnections {}
+
+    public object Any(ResetConnections request)
+    {
+        return Db.Column<bool>(
+            @"SELECT pg_terminate_backend(pid)
                     FROM pg_stat_activity
                     WHERE pid <> pg_backend_pid()
                       AND state = 'idle'
                       AND state_change < current_timestamp - INTERVAL '5' MINUTE");
-        }
     }
-
 }
