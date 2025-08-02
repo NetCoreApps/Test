049b0494d51f2b624c8a5c697776621f15bce5e2
Demis Bellot <demis.bellot@gmail.com>
2025-08-02 12:15:43 +0800

    Add TestUploadWithDto

--- Test.ServiceModel/FileUploads.cs
+++ Test.ServiceModel/FileUploads.cs
@@ -1,9 +1,11 @@
 #nullable enable
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using ServiceStack;
 using ServiceStack.DataAnnotations;
+using Test.ServiceModel.Types;
 
 namespace Test.ServiceModel;
 
@@ -100,4 +102,28 @@ public class TestFileUploadsResponse
     public string? RefId { get; set; }
     public List<UploadInfo> Files { get; set; } = [];
     public ResponseStatus? ResponseStatus { get; set; }
+}
+
+public class TestUploadWithDto : IPost, IReturn<TestUploadWithDto>
+{
+    public int Int { get; set; }
+    public int? NullableId { get; set; }
+    public long Long { get; set; }
+    public double Double { get; set; }
+    public string String { get; set; }
+    public DateTime DateTime { get; set; }
+    
+    public int[]? IntArray { get; set; }
+    public List<int>? IntList { get; set; }
+    public string[]? StringArray { get; set; }
+    public List<string>? StringList { get; set; }
+    public Poco[]? PocoArray { get; set; }
+    public List<Poco>? PocoList { get; set; }
+    public byte?[]? NullableByteArray { get; set; }
+    public List<byte?>? NullableByteList { get; set; }
+    public DateTime?[]? NullableDateTimeArray { get; set; }
+    public List<DateTime?>? NullableDateTimeList { get; set; }
+    public Dictionary<string, List<Poco>>? PocoLookup { get; set; }
+    public Dictionary<string, List<Dictionary<string,Poco>>>? PocoLookupMap { get; set; } 
+    public Dictionary<string, List<string>>? MapList { get; set; }
 }
